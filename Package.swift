8267502a7b2fd66da5e14f8fc8563a266ff6f6f8
mironal <miro.gunp@gmail.com>
2022-02-02 09:03:57 +0900

    define platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "TwitterAPIKit",
+    platforms: [
+        .macOS(.v10_14),
+        .iOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
