b8f3a0f33095ba51bdeb1df62ba39f20b34552b3
mironal <miro.gunp@gmail.com>
2022-02-03 09:17:58 +0900

    getShowStatus

---
+++ Sources/TwitterAPIKit/APIv1/Tweet/Requests/GetShowStatusRequestV1.swift
@@ -0,0 +1,50 @@
+// https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/api-reference/get-statuses-show-id
+
+import Foundation
+
+public class GetShowStatusRequestV1: TwitterAPIRequest {
+
+    public let id: String
+    public let trimUser: Bool?
+    public let includeMyRetweet: Bool?
+    public let includeEntities: Bool?
+    public let includeExtAltText: Bool?
+    public let includeCardUri: Bool?
+
+    var method: HTTPMethod {
+        return .get
+    }
+
+    var path: String {
+        return "/1.1/statuses/show.json"
+    }
+
+    var parameters: [String: Any]? {
+        var p = [String: Any]()
+
+        p["id"] = id
+        trimUser.map { p["trim_user"] = $0 }
+        includeMyRetweet.map { p["include_my_retweet"] = $0 }
+        includeEntities.map { p["include_entities"] = $0 }
+        includeExtAltText.map { p["include_ext_alt_text"] = $0 }
+        includeCardUri.map { p["include_card_uri"] = $0 }
+
+        return p
+    }
+
+    public init(
+        id: String,
+        trimUser: Bool? = nil,
+        includeMyRetweet: Bool? = nil,
+        includeEntities: Bool? = nil,
+        includeExtAltText: Bool? = nil,
+        includeCardUri: Bool? = nil
+    ) {
+        self.id = id
+        self.trimUser = trimUser
+        self.includeMyRetweet = includeMyRetweet
+        self.includeEntities = includeEntities
+        self.includeExtAltText = includeExtAltText
+        self.includeCardUri = includeCardUri
+    }
+}
--- Sources/TwitterAPIKit/APIv1/Tweet/TweetAPIv1.swift
+++ Sources/TwitterAPIKit/APIv1/Tweet/TweetAPIv1.swift
@@ -15,6 +15,13 @@ public protocol TweetAPIv1 {
         _ request: PostDestroyStatusRequestV1,
         completionHandler: @escaping (Result<(Data, HTTPURLResponse), TwitterAPIKitError>) -> Void
     ) -> URLSessionTask
+
+    // https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/api-reference/get-statuses-show-id
+    @discardableResult
+    func getShowStatus(
+        _ request: GetShowStatusRequestV1,
+        completionHandler: @escaping (Result<(Data, HTTPURLResponse), TwitterAPIKitError>) -> Void
+    ) -> URLSessionTask
 }
 
 extension TwitterAPIKit: TweetAPIv1 {
@@ -28,6 +35,13 @@ extension TwitterAPIKit: TweetAPIv1 {
 
     public func postDestroyStatus(
         _ request: PostDestroyStatusRequestV1,
+        completionHandler: @escaping (Result<(Data, HTTPURLResponse), TwitterAPIKitError>) -> Void
+    ) -> URLSessionTask {
+        return session.send(request, completionHandler: completionHandler)
+    }
+
+    public func getShowStatus(
+        _ request: GetShowStatusRequestV1,
         completionHandler: @escaping (Result<(Data, HTTPURLResponse), TwitterAPIKitError>) -> Void
     ) -> URLSessionTask {
         return session.send(request, completionHandler: completionHandler)
