941becccd26fabfac4a4b701a11675e0671c7bf0
mironal <miro.gunp@gmail.com>
2022-02-03 17:30:38 +0900

    getDirectMessaget

--- Sources/TwitterAPIKit/APIv1/DirectMessage/DirectMessageAPIv1.swift
+++ Sources/TwitterAPIKit/APIv1/DirectMessage/DirectMessageAPIv1.swift
@@ -3,8 +3,14 @@ import Foundation
 public protocol DirectMessageAPIv1 {
 
     /// https://developer.twitter.com/en/docs/twitter-api/v1/direct-messages/sending-and-receiving/api-reference/new-event
-    /// https://developer.twitter.com/en/docs/twitter-api/v1/direct-messages/sending-and-receiving/api-reference/get-event
     /// https://developer.twitter.com/en/docs/twitter-api/v1/direct-messages/sending-and-receiving/api-reference/delete-message-event
+
+    /// https://developer.twitter.com/en/docs/twitter-api/v1/direct-messages/sending-and-receiving/api-reference/get-event
+    @discardableResult
+    func getDirectMessage(
+        _ request: GetDirectMessageRequestV1,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> URLSessionTask
 
     /// https://developer.twitter.com/en/docs/twitter-api/v1/direct-messages/sending-and-receiving/api-reference/list-events
     @discardableResult
@@ -15,6 +21,14 @@ public protocol DirectMessageAPIv1 {
 }
 
 extension TwitterAPIKit: DirectMessageAPIv1 {
+
+    public func getDirectMessage(
+        _ request: GetDirectMessageRequestV1,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> URLSessionTask {
+        return session.send(request, completionHandler: completionHandler)
+    }
+
     public func getDirectMessageList(
         _ request: GetDirectMessageListRequestV1,
         completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
