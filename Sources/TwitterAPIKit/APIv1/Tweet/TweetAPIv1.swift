bf9b4675ae41d84edca7d5c05dee4ae651d43795
mironal <miro.gunp@gmail.com>
2022-02-06 18:33:08 +0900

    lookup

--- Sources/TwitterAPIKit/APIv1/Tweet/TweetAPIv1.swift
+++ Sources/TwitterAPIKit/APIv1/Tweet/TweetAPIv1.swift
@@ -16,10 +16,17 @@ public protocol TweetAPIv1 {
         completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
     ) -> URLSessionTask
 
-    // https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/api-reference/get-statuses-show-id
+    /// https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/api-reference/get-statuses-show-id
     @discardableResult
     func getShowStatus(
         _ request: GetShowStatusRequestV1,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> URLSessionTask
+
+    /// https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/api-reference/get-statuses-lookup
+    @discardableResult
+    func getLookupStatuses(
+        _ request: GetLookupStatusesRequestV1,
         completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
     ) -> URLSessionTask
 }
@@ -42,6 +49,13 @@ extension TwitterAPIKit: TweetAPIv1 {
 
     public func getShowStatus(
         _ request: GetShowStatusRequestV1,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> URLSessionTask {
+        return session.send(request, completionHandler: completionHandler)
+    }
+
+    public func getLookupStatuses(
+        _ request: GetLookupStatusesRequestV1,
         completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
     ) -> URLSessionTask {
         return session.send(request, completionHandler: completionHandler)
