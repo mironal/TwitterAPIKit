349f6b5a5dab554cb2f9b4c9d2616ee693782f63
mironal <miro.gunp@gmail.com>
2022-02-08 23:52:41 +0900

    getBlockIDs

--- Sources/TwitterAPIKit/APIv1/BlockAndMute/BlockAndMuteAPIv1.swift
+++ Sources/TwitterAPIKit/APIv1/BlockAndMute/BlockAndMuteAPIv1.swift
@@ -2,8 +2,20 @@ import Foundation
 
 public protocol BlockAndMuteAPIv1 {
 
+    /// https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/mute-block-report-users/api-reference/get-blocks-ids
+    func getBlockIDs(
+        _ request: GetBlocksIDsRequestV1,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> URLSessionTask
+
 }
 
 extension TwitterAPIKit: BlockAndMuteAPIv1 {
 
+    public func getBlockIDs(
+        _ request: GetBlocksIDsRequestV1,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> URLSessionTask {
+        return session.send(request, completionHandler: completionHandler)
+    }
 }
---
+++ Sources/TwitterAPIKit/APIv1/BlockAndMute/Requests/GetBlocksIDsRequestV1.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+/// https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/mute-block-report-users/api-reference/get-blocks-ids
+open class GetBlocksIDsRequestV1: TwitterAPIRequest {
+
+    public let stringifyIDs: Bool?
+    public let cursor: String?
+
+    public var method: HTTPMethod {
+        return .get
+    }
+    public var path: String {
+        return "/1.1/blocks/ids.json"
+    }
+
+    open var parameters: [String: Any]? {
+        var p = [String: Any]()
+        stringifyIDs.map { p["stringify_ids"] = $0 }
+        cursor.map { p["cursor"] = $0 }
+        return p
+    }
+    public init(
+        stringifyIDs: Bool? = .none,
+        cursor: String? = .none
+    ) {
+        self.stringifyIDs = stringifyIDs
+        self.cursor = cursor
+    }
+}
