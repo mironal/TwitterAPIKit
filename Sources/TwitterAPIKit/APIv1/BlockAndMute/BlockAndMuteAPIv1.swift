cfe3b83447c8f0b42d8296ad129beb784b501613
mironal <miro.gunp@gmail.com>
2022-02-08 23:58:47 +0900

    getBlockUsers

--- Sources/TwitterAPIKit/APIv1/BlockAndMute/BlockAndMuteAPIv1.swift
+++ Sources/TwitterAPIKit/APIv1/BlockAndMute/BlockAndMuteAPIv1.swift
@@ -8,12 +8,24 @@ public protocol BlockAndMuteAPIv1 {
         completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
     ) -> URLSessionTask
 
+    /// https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/mute-block-report-users/api-reference/get-blocks-list
+    func getBlockUsers(
+        _ request: GetBlocksListRequestV1,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> URLSessionTask
 }
 
 extension TwitterAPIKit: BlockAndMuteAPIv1 {
 
     public func getBlockIDs(
         _ request: GetBlocksIDsRequestV1,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> URLSessionTask {
+        return session.send(request, completionHandler: completionHandler)
+    }
+
+    public func getBlockUsers(
+        _ request: GetBlocksListRequestV1,
         completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
     ) -> URLSessionTask {
         return session.send(request, completionHandler: completionHandler)
---
+++ Sources/TwitterAPIKit/APIv1/BlockAndMute/Requests/GetBlocksListRequestV1.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+/// https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/mute-block-report-users/api-reference/get-blocks-list
+open class GetBlocksListRequestV1: TwitterAPIRequest {
+
+    public let includeEntities: Bool?
+    public let skipStatus: Bool?
+    public let cursor: String?
+
+    public var method: HTTPMethod {
+        return .get
+    }
+
+    public var path: String {
+        return "/1.1/blocks/list.json"
+    }
+
+    open var parameters: [String: Any]? {
+        var p = [String: Any]()
+        includeEntities.map { p["include_entities"] = $0 }
+        skipStatus.map { p["skip_status"] = $0 }
+        cursor.map { p["cursor"] = $0 }
+        return p
+    }
+
+    public init(
+        includeEntities: Bool? = .none,
+        skipStatus: Bool? = .none,
+        cursor: String? = .none
+    ) {
+        self.includeEntities = includeEntities
+        self.skipStatus = skipStatus
+        self.cursor = cursor
+    }
+}
