e79252412643f30d225168b8f9fe8a875e51da08
mironal <miro.gunp@gmail.com>
2022-02-09 09:04:31 +0900

    postBlock

--- Sources/TwitterAPIKit/APIv1/BlockAndMute/BlockAndMuteAPIv1.swift
+++ Sources/TwitterAPIKit/APIv1/BlockAndMute/BlockAndMuteAPIv1.swift
@@ -25,6 +25,13 @@ public protocol BlockAndMuteAPIv1 {
         _ request: GetMutesUsersListRequestV1,
         completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
     ) -> URLSessionTask
+
+    /// https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/mute-block-report-users/api-reference/post-blocks-create
+    func postBlock(
+        _ request: PostBlocksCreateRequestV1,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> URLSessionTask
+
 }
 
 extension TwitterAPIKit: BlockAndMuteAPIv1 {
@@ -52,6 +59,13 @@ extension TwitterAPIKit: BlockAndMuteAPIv1 {
 
     public func getMuteUsers(
         _ request: GetMutesUsersListRequestV1,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> URLSessionTask {
+        return session.send(request, completionHandler: completionHandler)
+    }
+
+    public func postBlock(
+        _ request: PostBlocksCreateRequestV1,
         completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
     ) -> URLSessionTask {
         return session.send(request, completionHandler: completionHandler)
