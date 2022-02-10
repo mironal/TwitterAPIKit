41b4b0225283ed1c615e18841ab2d92514947b46
mironal <miro.gunp@gmail.com>
2022-02-10 13:47:45 +0900

    searchUser

--- Sources/TwitterAPIKit/APIv1/Users/UserAPIv1.swift
+++ Sources/TwitterAPIKit/APIv1/Users/UserAPIv1.swift
@@ -15,6 +15,14 @@ public protocol UserAPIv1 {
         _ request: GetUsersShowRequestV1,
         completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
     ) -> URLSessionTask
+
+    /// https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/follow-search-get-users/api-reference/get-users-search
+    @discardableResult
+    func searchUser(
+        _ request: GetUsersShowRequestV1,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> URLSessionTask
+
 }
 
 extension TwitterAPIKit: UserAPIv1 {
@@ -32,4 +40,12 @@ extension TwitterAPIKit: UserAPIv1 {
     ) -> URLSessionTask {
         return session.send(request, completionHandler: completionHandler)
     }
+
+    public func searchUser(
+        _ request: GetUsersShowRequestV1,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> URLSessionTask {
+        return session.send(request, completionHandler: completionHandler)
+    }
+
 }
