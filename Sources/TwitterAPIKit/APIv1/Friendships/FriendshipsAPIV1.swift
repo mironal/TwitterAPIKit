edf57fe630010729ec8f825e6311251b6ab77331
mironal <miro.gunp@gmail.com>
2022-02-10 10:27:56 +0900

    getFollowerIDs

--- Sources/TwitterAPIKit/APIv1/Friendships/FriendshipsAPIV1.swift
+++ Sources/TwitterAPIKit/APIv1/Friendships/FriendshipsAPIV1.swift
@@ -2,8 +2,21 @@ import Foundation
 
 public protocol FriendshipsAPIV1 {
 
+    /// https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/follow-search-get-users/api-reference/get-followers-ids
+    @discardableResult
+    func getFollowerIDs(
+        _ request: GetFollowersIDsRequestV1,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> URLSessionTask
+
 }
 
 extension TwitterAPIKit: FriendshipsAPIV1 {
 
+    public func getFollowerIDs(
+        _ request: GetFollowersIDsRequestV1,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> URLSessionTask {
+        return session.send(request, completionHandler: completionHandler)
+    }
 }
