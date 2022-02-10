b18eadb097dc6c73a15bf26d20cc78bd1a5612e8
mironal <miro.gunp@gmail.com>
2022-02-10 22:57:51 +0900

    getTrendsClosest

---
+++ Sources/TwitterAPIKit/APIv1/Trend/Requests/GetTrendsClosestRequestV1.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+/// https://developer.twitter.com/en/docs/twitter-api/v1/trends/locations-with-trending-topics/api-reference/get-trends-closest
+open class GetTrendsClosestRequestV1: TwitterAPIRequest {
+
+    public struct Coordinate {
+        var lat: Double
+        var long: Double
+    }
+
+    public let location: Coordinate
+
+    public var method: HTTPMethod {
+        return .get
+    }
+
+    public var path: String {
+        return "/1.1/trends/closest.json"
+    }
+
+    open var parameters: [String: Any] {
+        return [
+            "lat": location.lat,
+            "long": location.long,
+        ]
+    }
+
+    public init(
+        location: Coordinate
+    ) {
+        self.location = location
+    }
+
+}
--- Sources/TwitterAPIKit/APIv1/Trend/TrendAPIv1.swift
+++ Sources/TwitterAPIKit/APIv1/Trend/TrendAPIv1.swift
@@ -9,12 +9,26 @@ public protocol TrendAPIv1 {
         completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
     ) -> URLSessionTask
 
+    /// https://developer.twitter.com/en/docs/twitter-api/v1/trends/locations-with-trending-topics/api-reference/get-trends-closest
+    @discardableResult
+    func getTrendsClosest(
+        _ request: GetTrendsClosestRequestV1,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> URLSessionTask
+
 }
 
 extension TwitterAPIKit: TrendAPIv1 {
 
     public func getTrendsAvailable(
         _ request: GetTrendsAvailableRequestV1,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> URLSessionTask {
+        return session.send(request, completionHandler: completionHandler)
+    }
+
+    public func getTrendsClosest(
+        _ request: GetTrendsClosestRequestV1,
         completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
     ) -> URLSessionTask {
         return session.send(request, completionHandler: completionHandler)
