7518a72d3f3af8c8c9664f2aee229031d2eebab7
mironal <miro.gunp@gmail.com>
2022-02-12 00:43:57 +0900

    getAccountVerify

--- Sources/TwitterAPIKit/APIv1/Account/AccountAPIv1.swift
+++ Sources/TwitterAPIKit/APIv1/Account/AccountAPIv1.swift
@@ -7,11 +7,24 @@ public protocol AccountAPIv1 {
         _ request: GetAccountSettingsRequestV1,
         completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
     ) -> URLSessionTask
+
+    /// https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/manage-account-settings/api-reference/get-account-verify_credentials
+    func getAccountVerify(
+        _ request: GetAccountVerifyCredentialsRequestV1,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> URLSessionTask
 }
 
 extension TwitterAPIKit: AccountAPIv1 {
     public func getAccountSetting(
         _ request: GetAccountSettingsRequestV1,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> URLSessionTask {
+        return session.send(request, completionHandler: completionHandler)
+    }
+
+    public func getAccountVerify(
+        _ request: GetAccountVerifyCredentialsRequestV1,
         completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
     ) -> URLSessionTask {
         return session.send(request, completionHandler: completionHandler)
