a3ccb27d1ab5c2544118f729214d29c537fad1a7
mironal <miro.gunp@gmail.com>
2022-02-12 14:58:23 +0900

    postOAuthRequestToken

--- Sources/TwitterAPIKit/APIv1/Auth/AuthAPIv1.swift
+++ Sources/TwitterAPIKit/APIv1/Auth/AuthAPIv1.swift
@@ -2,8 +2,19 @@ import Foundation
 
 public protocol AuthAPIv1 {
 
+    /// https://developer.twitter.com/en/docs/authentication/api-reference/request_token
+    @discardableResult
+    func postOAuthRequestToken(
+        _ request: PostOAuthRequestTokenRequestV1,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> TwitterAPISessionTask
 }
 
 extension TwitterAPIKit: AuthAPIv1 {
-
+    public func postOAuthRequestToken(
+        _ request: PostOAuthRequestTokenRequestV1,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> TwitterAPISessionTask {
+        return session.send(request, completionHandler: completionHandler)
+    }
 }
