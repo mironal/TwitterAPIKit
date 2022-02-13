f9ee78db4f46b0cdb8072cf492f31f8ac4e0afe7
mironal <miro.gunp@gmail.com>
2022-02-13 20:26:56 +0900

    getLikingUsers

--- Sources/TwitterAPIKit/APIv2/Like/LikeAPIv2.swift
+++ Sources/TwitterAPIKit/APIv2/Like/LikeAPIv2.swift
@@ -2,8 +2,19 @@ import Foundation
 
 public protocol LikeAPIv2 {
 
+    /// https://developer.twitter.com/en/docs/twitter-api/tweets/likes/api-reference/get-tweets-id-liking_users
+    @discardableResult
+    func getLikingUsers(
+        _ request: GetTweetsLikingUsersRequestV2,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> TwitterAPISessionTask
 }
 
 extension TwitterAPIKit: LikeAPIv2 {
-
+    public func getLikingUsers(
+        _ request: GetTweetsLikingUsersRequestV2,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> TwitterAPISessionTask {
+        return session.send(request, completionHandler: completionHandler)
+    }
 }
