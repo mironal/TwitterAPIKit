fc475ab6ca8245a994f0461c0d5f1c62a82d0d7c
mironal <miro.gunp@gmail.com>
2022-02-13 19:22:46 +0900

    getRetweetedBy

--- Sources/TwitterAPIKit/APIv2/Retweet/RetweetAPIv2.swift
+++ Sources/TwitterAPIKit/APIv2/Retweet/RetweetAPIv2.swift
@@ -2,8 +2,21 @@ import Foundation
 
 public protocol RetweetAPIv2 {
 
+    /// https://developer.twitter.com/en/docs/twitter-api/tweets/retweets/api-reference/get-tweets-id-retweeted_by
+    @discardableResult
+    func getRetweetedBy(
+        _ request: GetTweetsRetweetedByRequestV2,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> TwitterAPISessionTask
+
 }
 
 extension TwitterAPIKit: RetweetAPIv2 {
 
+    public func getRetweetedBy(
+        _ request: GetTweetsRetweetedByRequestV2,
+        completionHandler: @escaping (Result<TwitterAPISuccessReponse, TwitterAPIKitError>) -> Void
+    ) -> TwitterAPISessionTask {
+        return session.send(request, completionHandler: completionHandler)
+    }
 }
