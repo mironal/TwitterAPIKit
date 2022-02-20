1343ac926958f6b9712f24f70465252812c12f40
mironal <miro.gunp@gmail.com>
2022-02-20 14:03:35 +0900

    GetTweetsCountsRecentRequestV2

--- Sources/TwitterAPIKit/APIv2/TweetCount/TweetCountAPIv2.swift
+++ Sources/TwitterAPIKit/APIv2/TweetCount/TweetCountAPIv2.swift
@@ -1,9 +1,16 @@
 import Foundation
 
 public protocol TweetCountAPIv2 {
-
+    /// https://developer.twitter.com/en/docs/twitter-api/tweets/counts/api-reference/get-tweets-counts-recent
+    func getTweetCountRecent(
+        _ request: GetTweetsCountsRecentRequestV2
+    ) -> TwitterAPISessionJSONTask
 }
 
 extension TwitterAPIKit.TwitterAPIImplV2: TweetCountAPIv2 {
-
+    func getTweetCountRecent(
+        _ request: GetTweetsCountsRecentRequestV2
+    ) -> TwitterAPISessionJSONTask {
+        return session.send(request)
+    }
 }
