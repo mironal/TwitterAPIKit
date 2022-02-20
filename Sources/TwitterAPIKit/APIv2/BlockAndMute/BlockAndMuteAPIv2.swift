b2c9ff8b7599e85fe04861dd688b462868e0f4d4
mironal <miro.gunp@gmail.com>
2022-02-20 14:42:43 +0900

    GetUsersBlockingRequestV2

--- Sources/TwitterAPIKit/APIv2/BlockAndMute/BlockAndMuteAPIv2.swift
+++ Sources/TwitterAPIKit/APIv2/BlockAndMute/BlockAndMuteAPIv2.swift
@@ -2,8 +2,15 @@ import Foundation
 
 public protocol BlockAndMuteAPIv2 {
 
+    /// https://developer.twitter.com/en/docs/twitter-api/users/blocks/api-reference/get-users-blocking
+    func getBlockUsers(
+        _ request: GetUsersBlockingRequestV2
+    ) -> TwitterAPISessionJSONTask
 }
 
 extension TwitterAPIKit.TwitterAPIImplV2: BlockAndMuteAPIv2 {
 
+    func getBlockUsers(_ request: GetUsersBlockingRequestV2) -> TwitterAPISessionJSONTask {
+        return session.send(request)
+    }
 }
