505204a4bfbee3f088764dfa74fa6aab993adf27
mironal <miro.gunp@gmail.com>
2022-02-20 15:47:55 +0900

    GetUsersFollowingRequestV2

--- Sources/TwitterAPIKit/APIv2/Friendships/FriendshipsAPIv2.swift
+++ Sources/TwitterAPIKit/APIv2/Friendships/FriendshipsAPIv2.swift
@@ -1,9 +1,16 @@
 import Foundation
 
 public protocol FriendshipsAPIv2 {
-
+    /// https://developer.twitter.com/en/docs/twitter-api/users/follows/api-reference/get-users-id-following
+    func getFollowing(
+        _ request: GetUsersFollowingRequestV2
+    ) -> TwitterAPISessionJSONTask
 }
 
 extension TwitterAPIKit.TwitterAPIImplV2: FriendshipsAPIv2 {
+
+    func getFollowing(_ request: GetUsersFollowingRequestV2) -> TwitterAPISessionJSONTask {
+        return session.send(request)
+    }
 
 }
