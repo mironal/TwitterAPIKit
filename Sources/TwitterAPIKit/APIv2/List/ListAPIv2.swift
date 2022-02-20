c87bc87361319b15a1d2c63eab8db23bf2e4c1fc
mironal <miro.gunp@gmail.com>
2022-02-20 16:20:16 +0900

    GetListsTweetsRequestV2

--- Sources/TwitterAPIKit/APIv2/List/ListAPIv2.swift
+++ Sources/TwitterAPIKit/APIv2/List/ListAPIv2.swift
@@ -2,8 +2,15 @@ import Foundation
 
 public protocol ListAPIv2 {
 
+    /// https://developer.twitter.com/en/docs/twitter-api/lists/list-tweets/api-reference/get-lists-id-tweets
+    func getListTweets(
+        _ request: GetListsTweetsRequestV2
+    ) -> TwitterAPISessionJSONTask
 }
 
 extension TwitterAPIKit.TwitterAPIImplV2: ListAPIv2 {
 
+    func getListTweets(_ request: GetListsTweetsRequestV2) -> TwitterAPISessionJSONTask {
+        return session.send(request)
+    }
 }
