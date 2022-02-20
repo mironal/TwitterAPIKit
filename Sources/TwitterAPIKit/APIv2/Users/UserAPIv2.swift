b6349e6a17657e68f5a8e814f79e8a0e90445e91
mironal <miro.gunp@gmail.com>
2022-02-20 15:16:56 +0900

    GetUserRequestV2

--- Sources/TwitterAPIKit/APIv2/Users/UserAPIv2.swift
+++ Sources/TwitterAPIKit/APIv2/Users/UserAPIv2.swift
@@ -1,9 +1,16 @@
 import Foundation
 
 public protocol UserAPIv2 {
+    /// https://developer.twitter.com/en/docs/twitter-api/users/lookup/api-reference/get-users-id
+    func getUser(
+        _ request: GetUserRequestV2
+    ) -> TwitterAPISessionJSONTask
 
 }
 
 extension TwitterAPIKit.TwitterAPIImplV2: UserAPIv2 {
 
+    func getUser(_ request: GetUserRequestV2) -> TwitterAPISessionJSONTask {
+        return session.send(request)
+    }
 }
