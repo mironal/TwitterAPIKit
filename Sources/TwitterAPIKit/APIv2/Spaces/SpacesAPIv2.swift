b9331982218872aa32d1c70b29ebe5eacf16e802
mironal <miro.gunp@gmail.com>
2022-02-21 22:38:52 +0900

    GetSpaceRequestV2

--- Sources/TwitterAPIKit/APIv2/Spaces/SpacesAPIv2.swift
+++ Sources/TwitterAPIKit/APIv2/Spaces/SpacesAPIv2.swift
@@ -2,8 +2,14 @@ import Foundation
 
 public protocol SpacesAPIv2 {
 
+    /// https://developer.twitter.com/en/docs/twitter-api/spaces/lookup/api-reference/get-spaces-id
+    func getSpace(
+        _ request: GetSpaceRequestV2
+    ) -> TwitterAPISessionJSONTask
 }
 
 extension TwitterAPIKit.TwitterAPIImplV2: SpacesAPIv2 {
-
+    func getSpace(_ request: GetSpaceRequestV2) -> TwitterAPISessionJSONTask {
+        return session.send(request)
+    }
 }
