63f7892f3c6068cfb2ee77964d21a576b1d95033
mironal <miro.gunp@gmail.com>
2022-02-22 09:50:55 +0900

    add SearchAPIv2

---
+++ Sources/TwitterAPIKit/APIv2/Search/SearchAPIv2.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public protocol SearchAPIv2 {
+
+}
+
+extension TwitterAPIKit.TwitterAPIImplV2: SearchAPIv2 {
+
+}
--- Sources/TwitterAPIKit/APIv2/TwitterAPIv2.swift
+++ Sources/TwitterAPIKit/APIv2/TwitterAPIv2.swift
@@ -10,6 +10,7 @@ public typealias TwitterAPIv2 =
     & LikeAPIv2
     & ListAPIv2
     & RetweetAPIv2
+    & SearchAPIv2
     & SpacesAPIv2
     & TimelineAPIv2
     & TweetAPIv2
@@ -23,6 +24,7 @@ public protocol TwitterAPIResourceV2 {
     var like: LikeAPIv2 { get }
     var list: ListAPIv2 { get }
     var retweet: RetweetAPIv2 { get }
+    var search: SearchAPIv2 { get }
     var spaces: SpacesAPIv2 { get }
     var timeline: TimelineAPIv2 { get }
     var tweet: TweetAPIv2 { get }
@@ -37,6 +39,7 @@ extension TwitterAPIKit.TwitterAPIImplV2: TwitterAPIResourceV2 {
     var like: LikeAPIv2 { return self }
     var list: ListAPIv2 { return self }
     var retweet: RetweetAPIv2 { return self }
+    var search: SearchAPIv2 { return self }
     var spaces: SpacesAPIv2 { return self }
     var timeline: TimelineAPIv2 { return self }
     var tweet: TweetAPIv2 { return self }
