6f7076c6935f8e2d3956fcc9b4417f971ab043b1
mironal <miro.gunp@gmail.com>
2022-02-22 10:08:27 +0900

    GetTweetsSearchRecentRequestV2

--- Sources/TwitterAPIKit/APIv2/Search/SearchAPIv2.swift
+++ Sources/TwitterAPIKit/APIv2/Search/SearchAPIv2.swift
@@ -2,8 +2,14 @@ import Foundation
 
 public protocol SearchAPIv2 {
 
+    /// https://developer.twitter.com/en/docs/twitter-api/tweets/search/api-reference/get-tweets-search-recent
+    func searchTweets(
+        _ request: GetTweetsSearchRecentRequestV2
+    ) -> TwitterAPISessionJSONTask
 }
 
 extension TwitterAPIKit.TwitterAPIImplV2: SearchAPIv2 {
-
+    func searchTweets(_ request: GetTweetsSearchRecentRequestV2) -> TwitterAPISessionJSONTask {
+        return session.send(request)
+    }
 }
