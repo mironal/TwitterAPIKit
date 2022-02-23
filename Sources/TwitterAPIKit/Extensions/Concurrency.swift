63883a8a293adfa6bc8ec2048817c0f5bd11866b
mironal <miro.gunp@gmail.com>
2022-02-23 18:53:07 +0900

    Concurrency

--- Sources/TwitterAPIKit/Extensions/Concurrency.swift
+++ Sources/TwitterAPIKit/Extensions/Concurrency.swift
@@ -4,49 +4,18 @@ import Foundation
 
 #if compiler(>=5.5.2) && canImport(_Concurrency)
 
-    @available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
-    extension TwitterAPISessionJSONTask {
+// I'm not that familiar with Swift Concurrency, so please report any problems.
 
-        // Currently, queue cannot be passed.
+@available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
+extension TwitterAPISessionDataTask {
 
-        public var responseData: TwitterAPIResponse<Data> {
-            get async throws {
-                return try await withTaskCancellationHandler(
-                    operation: {
-                        return try await withUnsafeThrowingContinuation { c in
-                            responseData { result in
-                                c.resume(returning: result)
-                            }
-                        }
-                    },
-                    onCancel: {
-                        cancel()
-                    })
-            }
-        }
-
-        public var responseObject: TwitterAPIResponse<Any> {
-            get async throws {
-                return try await withTaskCancellationHandler(
-                    operation: {
-                        return try await withUnsafeThrowingContinuation { c in
-                            responseObject { result in
-                                c.resume(returning: result)
-                            }
-                        }
-                    },
-                    onCancel: {
-                        cancel()
-                    })
-            }
-        }
-
-        public func responseDecodable<T: Decodable>(type: T.Type) async throws -> TwitterAPIResponse<T> {
-            return try await withTaskCancellationHandler(
+    public var responseData: TwitterAPIResponse<Data> {
+        get async {
+            return await withTaskCancellationHandler(
                 operation: {
-                    return try await withUnsafeThrowingContinuation { c in
-                        responseDecodable(type: type) { result in
-                            c.resume(returning: result)
+                    return await withCheckedContinuation { c in
+                        responseData { response in
+                            c.resume(returning: response)
                         }
                     }
                 },
@@ -55,4 +24,59 @@ import Foundation
                 })
         }
     }
+}
+
+@available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
+extension TwitterAPISessionJSONTask {
+
+    public var responseObject: TwitterAPIResponse<Any> {
+        get async {
+            return await withTaskCancellationHandler(
+                operation: {
+                    return await withCheckedContinuation { c in
+                        responseObject { response in
+                            c.resume(returning: response)
+                        }
+                    }
+                },
+                onCancel: {
+                    cancel()
+                })
+        }
+    }
+
+    public func responseDecodable<T: Decodable>(type: T.Type) async -> TwitterAPIResponse<T> {
+        return await withTaskCancellationHandler(
+            operation: {
+                return await withCheckedContinuation { c in
+                    responseDecodable(type: type) { response in
+                        c.resume(returning: response)
+                    }
+                }
+            },
+            onCancel: {
+                cancel()
+            })
+    }
+}
+
+@available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
+extension TwitterAPISessionSpecializedTask {
+
+    public var responseObject: TwitterAPIResponse<Success> {
+        get async {
+            return await withTaskCancellationHandler(
+                operation: {
+                    return await withCheckedContinuation { c in
+                        responseObject { response in
+                            c.resume(returning: response)
+                        }
+                    }
+                }, onCancel: {
+                    cancel()
+                })
+        }
+    }
+}
+
 #endif
