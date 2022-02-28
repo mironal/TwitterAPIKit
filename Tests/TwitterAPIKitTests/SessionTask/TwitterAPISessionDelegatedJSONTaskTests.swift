db028fc82d82cb16c2a5974e56c20f420413f39c
mironal <miro.gunp@gmail.com>
2022-02-28 13:12:18 +0900

    more test

--- Tests/TwitterAPIKitTests/SessionTask/TwitterAPISessionDelegatedJSONTaskTests.swift
+++ Tests/TwitterAPIKitTests/SessionTask/TwitterAPISessionDelegatedJSONTaskTests.swift
@@ -84,4 +84,79 @@ class TwitterAPISessionDelegatedJSONTaskTests: XCTestCase {
 
         wait(for: [exp], timeout: 10)
     }
+
+    func testCompleteWithError() throws {
+
+        let mockTask = MockTwitterAPISessionTask(taskIdentifier: 1)
+
+        let task = TwitterAPISessionDelegatedJSONTask(
+            task: mockTask
+        )
+
+        let exp = expectation(description: "")
+        exp.expectedFulfillmentCount = 3
+
+        _ = task.responseData(queue: .global(qos: .background)) { response in
+            XCTAssertTrue(response.isError)
+            XCTAssertEqual(response.error?.underlyingError as! URLError, URLError(.badServerResponse))
+            exp.fulfill()
+        }.responseObject(queue: .global(qos: .background)) { response in
+            XCTAssertTrue(response.isError)
+            exp.fulfill()
+        }.responseDecodable(type: DecodableObj.self, queue: .global(qos: .background)) { response in
+            XCTAssertTrue(response.isError)
+            exp.fulfill()
+        }
+
+        DispatchQueue.global(qos: .background).async {
+            task.append(chunk: Data("{\"key\"".utf8))
+            task.append(chunk: Data(":\"value\"}".utf8))
+
+            mockTask.httpResponse = .init(
+                url: URL(string: "http://example.com")!, statusCode: 200, httpVersion: "1.1", headerFields: [:])
+
+            task.complete(error: URLError.init(.badServerResponse))
+        }
+
+        wait(for: [exp], timeout: 10)
+    }
+
+    func testCancel() throws {
+
+        let mockTask = MockTwitterAPISessionTask(taskIdentifier: 1)
+
+        let task = TwitterAPISessionDelegatedJSONTask(
+            task: mockTask
+        )
+
+        let exp = expectation(description: "")
+        exp.expectedFulfillmentCount = 3
+
+        _ = task.responseData(queue: .global(qos: .background)) { response in
+            XCTAssertTrue(response.isError)
+            XCTAssertEqual(response.error?.underlyingError as! URLError, URLError(.cancelled))
+            exp.fulfill()
+        }.responseObject(queue: .global(qos: .background)) { response in
+            XCTAssertTrue(response.isError)
+            exp.fulfill()
+        }.responseDecodable(type: DecodableObj.self, queue: .global(qos: .background)) { response in
+            XCTAssertTrue(response.isError)
+            exp.fulfill()
+        }
+
+        DispatchQueue.global(qos: .background).async {
+            task.append(chunk: Data("{\"key\"".utf8))
+            task.append(chunk: Data(":\"value\"}".utf8))
+
+            mockTask.httpResponse = .init(
+                url: URL(string: "http://example.com")!, statusCode: 200, httpVersion: "1.1", headerFields: [:])
+
+            task.complete(error: URLError.init(.cancelled))
+        }
+
+        task.cancel()
+
+        wait(for: [exp], timeout: 10)
+        XCTAssertTrue(mockTask.cancelled)
+    }
 }
