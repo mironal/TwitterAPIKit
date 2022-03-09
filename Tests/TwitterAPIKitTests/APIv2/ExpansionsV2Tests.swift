3b2032d5ef91bda94d021f4cb21fc3162413c806
mironal <miro.gunp@gmail.com>
2022-03-09 17:01:00 +0900

    test TwitterUserExpansionsV2

--- Tests/TwitterAPIKitTests/APIv2/ExpansionsV2Tests.swift
+++ Tests/TwitterAPIKitTests/APIv2/ExpansionsV2Tests.swift
@@ -29,4 +29,18 @@ class ExpansionsV2Tests: XCTestCase {
             "attachments.media_keys,attachments.poll_ids,author_id,entities.mentions.username,geo.place_id,in_reply_to_user_id,referenced_tweets.id,referenced_tweets.id.author_id,~~~"
         )
     }
+
+    func testTwitterUserExpansionsV2() throws {
+
+        let allCases: [TwitterUserExpansionsV2] = [
+            .pinnedTweetID,
+            .other("~~~"),
+        ]
+
+        // curl https://api.twitter.com/2/openapi.json | jq '.components.parameters.UserExpansionsParameter.schema.items.enum | sort | join(",")'
+        XCTAssertEqual(
+            allCases.commaSeparatedString,
+            "pinned_tweet_id,~~~"
+        )
+    }
 }
