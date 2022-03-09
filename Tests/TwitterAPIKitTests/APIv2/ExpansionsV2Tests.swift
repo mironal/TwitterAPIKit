5877018dbe8c0e486e92bc0b03adaee34c9496e8
mironal <miro.gunp@gmail.com>
2022-03-09 16:58:13 +0900

    test TwitterTweetExpansionsV2

---
+++ Tests/TwitterAPIKitTests/APIv2/ExpansionsV2Tests.swift
@@ -0,0 +1,32 @@
+import XCTest
+
+@testable import TwitterAPIKit
+
+class ExpansionsV2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testTwitterTweetExpansionsV2() throws {
+        let allCases: [TwitterTweetExpansionsV2] = [
+            .attachmentsPollIDs,
+            .attachmentsMediaKeys,
+            .authorID,
+            .entitiesMentionsUsername,
+            .geoPlaceID,
+            .inReplyToUserID,
+            .referencedTweetsID,
+            .referencedTweetsIDAuthorID,
+            .other("~~~"),
+        ]
+
+        // curl https://api.twitter.com/2/openapi.json | jq '.components.parameters.TweetExpansionsParameter.schema.items.enum | sort | join(",")'
+        XCTAssertEqual(
+            allCases.commaSeparatedString,
+            "attachments.media_keys,attachments.poll_ids,author_id,entities.mentions.username,geo.place_id,in_reply_to_user_id,referenced_tweets.id,referenced_tweets.id.author_id,~~~"
+        )
+    }
+}
