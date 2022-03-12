82794020f37052fc3850166769c264c56684768b
mironal <miro.gunp@gmail.com>
2022-03-12 22:41:16 +0900

    test PostMutesUsersCreateRequestV1

---
+++ Tests/TwitterAPIKitTests/APIv1/BlockAndMute/PostMutesUsersCreateRequestV1Tests.swift
@@ -0,0 +1,40 @@
+import TwitterAPIKit
+import XCTest
+
+class PostMutesUsersCreateRequestV1Tests: XCTestCase {
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func test() throws {
+        let req = PostMutesUsersCreateRequestV1(
+            user: .userID("uid")
+        )
+
+        XCTAssertEqual(req.method, .post)
+        XCTAssertEqual(req.baseURLType, .api)
+        XCTAssertEqual(req.path, "/1.1/mutes/users/create.json")
+        XCTAssertEqual(req.bodyContentType, .wwwFormUrlEncoded)
+        AssertEqualAnyDict(
+            req.parameters,
+            [
+                "user_id": "uid"
+            ]
+        )
+    }
+
+    func testScreenName() throws {
+        let req = PostMutesUsersCreateRequestV1(
+            user: .screenName("s")
+        )
+
+        AssertEqualAnyDict(
+            req.parameters,
+            [
+                "screen_name": "s"
+            ]
+        )
+    }
+}
