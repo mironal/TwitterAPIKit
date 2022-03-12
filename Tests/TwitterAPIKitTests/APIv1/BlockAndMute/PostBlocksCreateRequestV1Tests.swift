3675053e555242a26f9212cf4f7ee04eff5db957
mironal <miro.gunp@gmail.com>
2022-03-12 22:38:08 +0900

    test PostBlocksCreateRequestV1

---
+++ Tests/TwitterAPIKitTests/APIv1/BlockAndMute/PostBlocksCreateRequestV1Tests.swift
@@ -0,0 +1,44 @@
+import TwitterAPIKit
+import XCTest
+
+class PostBlocksCreateRequestV1Tests: XCTestCase {
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func test() throws {
+        let req = PostBlocksCreateRequestV1(
+            user: .userID("uid"),
+            includeEntities: true,
+            skipStatus: true
+        )
+
+        XCTAssertEqual(req.method, .post)
+        XCTAssertEqual(req.baseURLType, .api)
+        XCTAssertEqual(req.path, "/1.1/blocks/create.json")
+        XCTAssertEqual(req.bodyContentType, .wwwFormUrlEncoded)
+        AssertEqualAnyDict(
+            req.parameters,
+            [
+                "user_id": "uid",
+                "include_entities": true,
+                "skip_status": true,
+            ]
+        )
+    }
+
+    func testDefaultArg() throws {
+        let req = PostBlocksCreateRequestV1(
+            user: .screenName("s")
+        )
+
+        AssertEqualAnyDict(
+            req.parameters,
+            [
+                "screen_name": "s"
+            ]
+        )
+    }
+}
