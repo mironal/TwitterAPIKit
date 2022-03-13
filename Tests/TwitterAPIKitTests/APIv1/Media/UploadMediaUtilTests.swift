34d9e0f1a36075b6c9b7b3339174bf0aa1777fc6
mironal <miro.gunp@gmail.com>
2022-03-13 20:50:22 +0900

    split

--- Tests/TwitterAPIKitTests/APIv1/Media/UploadMediaUtilTests.swift
+++ Tests/TwitterAPIKitTests/APIv1/Media/UploadMediaUtilTests.swift
@@ -20,7 +20,114 @@ class UploadMediaUtilTests: XCTestCase {
             configuration: config
         )
 
-        let processingSuccess = Bool.random()
+        var requestCount = 0
+        MockURLProtocol.requestHandler = { request in
+            defer {
+                requestCount += 1
+            }
+            let requestData = request.httpBodyStream.flatMap {
+                try? Data(reading: $0)
+            }
+            let requestBodyString = requestData.flatMap {
+                String(data: $0, encoding: .utf8)
+            }
+            var data = Data()
+            switch requestCount {
+            case 0:  // INIT
+                XCTAssertEqual(request.url?.absoluteString, "https://upload.twitter.com/1.1/media/upload.json")
+                XCTAssertTrue(requestBodyString!.contains("command=INIT"))
+
+                data = try! JSONSerialization.data(
+                    withJSONObject: [
+                        "media_id_string": "123",
+                        "expires_after_secs": 1000,
+                    ], options: [])
+
+            case 1:  // APPEND
+                XCTAssertEqual(request.url?.absoluteString, "https://upload.twitter.com/1.1/media/upload.json")
+                XCTAssertTrue(requestBodyString!.contains("\r\nAPPEND\r\n"))
+            case 2:  // APPEND
+                XCTAssertEqual(request.url?.absoluteString, "https://upload.twitter.com/1.1/media/upload.json")
+                XCTAssertTrue(requestBodyString!.contains("\r\nAPPEND\r\n"))
+            case 3:  // FINALIZE
+                XCTAssertEqual(request.url?.absoluteString, "https://upload.twitter.com/1.1/media/upload.json")
+                XCTAssertTrue(requestBodyString!.contains("command=FINALIZE"))
+
+                data = try! JSONSerialization.data(
+                    withJSONObject: [
+                        "media_id_string": "123",
+                        "size": 10,
+                        "expires_after_secs": 200,
+                        "processingInfo": [
+                            "state": "pending",
+                            "check_after_secs": 0,
+                        ],
+                    ], options: [])
+            case 4:
+
+                XCTAssertEqual(request.httpMethod, "GET")
+                XCTAssertEqual(request.url?.path, "/1.1/media/upload.json")
+                XCTAssertTrue(request.url!.query!.contains("command=STATUS"))
+
+                data = try! JSONSerialization.data(
+                    withJSONObject: [
+                        "media_id_string": "123",
+                        "processing_info": [
+                            "state": "in_progress",
+                            "progress_percent": 99,
+                            "check_after_secs": 0,
+                        ],
+                    ], options: []
+                )
+            case 5:
+
+                XCTAssertEqual(request.httpMethod, "GET")
+                XCTAssertEqual(request.url?.path, "/1.1/media/upload.json")
+                XCTAssertTrue(request.url!.query!.contains("command=STATUS"))
+
+                data = try! JSONSerialization.data(
+                    withJSONObject: [
+                        "media_id_string": "123",
+                        "video": [
+                            "video_type": "video/mp4"
+                        ],
+                        "processing_info": [
+                            "state": "succeeded",
+                            "progress_percent": 100,
+                        ],
+                    ], options: []
+                )
+            default:
+                XCTFail()
+            }
+
+            return (
+                HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: "2.0", headerFields: [:])!,
+                data
+            )
+        }
+
+        let exp = expectation(description: "")
+        let data = Data([1, 2, 3])
+        client.v1.media.uploadMedia(.init(media: data, mediaType: "m", filename: "f", uploadChunkSize: 2)) { response in
+
+            XCTAssertFalse(response.isError)
+            XCTAssertEqual(response.success, "123")
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 10)
+    }
+
+    func testWithProcessingError() throws {
+
+        let config = URLSessionConfiguration.default
+        config.protocolClasses = [MockURLProtocol.self]
+
+        let client = TwitterAPIKit(
+            .oauth(consumerKey: "", consumerSecret: "", oauthToken: "", oauthTokenSecret: ""),
+            configuration: config
+        )
 
         var requestCount = 0
         MockURLProtocol.requestHandler = { request in
@@ -87,35 +194,20 @@ class UploadMediaUtilTests: XCTestCase {
                 XCTAssertEqual(request.url?.path, "/1.1/media/upload.json")
                 XCTAssertTrue(request.url!.query!.contains("command=STATUS"))
 
-                if processingSuccess {
-                    data = try! JSONSerialization.data(
-                        withJSONObject: [
-                            "media_id_string": "123",
-                            "video": [
-                                "video_type": "video/mp4"
+                data = try! JSONSerialization.data(
+                    withJSONObject: [
+                        "media_id_string": "123",
+                        "processing_info": [
+                            "state": "failed",
+                            "progress_percent": 99,
+                            "error": [
+                                "code": 1,
+                                "name": "InvalidMedia",
+                                "message": "Unsupported video format",
                             ],
-                            "processing_info": [
-                                "state": "succeeded",
-                                "progress_percent": 100,
-                            ],
-                        ], options: []
-                    )
-                } else {
-                    data = try! JSONSerialization.data(
-                        withJSONObject: [
-                            "media_id_string": "123",
-                            "processing_info": [
-                                "state": "failed",
-                                "progress_percent": 99,
-                                "error": [
-                                    "code": 1,
-                                    "name": "InvalidMedia",
-                                    "message": "Unsupported video format",
-                                ],
-                            ],
-                        ], options: []
-                    )
-                }
+                        ],
+                    ], options: []
+                )
             default:
                 XCTFail()
             }
@@ -129,14 +221,8 @@ class UploadMediaUtilTests: XCTestCase {
         let exp = expectation(description: "")
         let data = Data([1, 2, 3])
         client.v1.media.uploadMedia(.init(media: data, mediaType: "m", filename: "f", uploadChunkSize: 2)) { response in
-            if processingSuccess {
-
-                XCTAssertFalse(response.isError)
-                XCTAssertEqual(response.success, "123")
-            } else {
-                XCTAssertTrue(response.isError)
-                XCTAssertTrue(response.error!.isUploadMediaFailed)
-            }
+            XCTAssertTrue(response.isError)
+            XCTAssertTrue(response.error!.isUploadMediaFailed)
             exp.fulfill()
         }
 
@@ -210,5 +296,4 @@ class UploadMediaUtilTests: XCTestCase {
 
         wait(for: [exp], timeout: 10)
     }
-
 }
