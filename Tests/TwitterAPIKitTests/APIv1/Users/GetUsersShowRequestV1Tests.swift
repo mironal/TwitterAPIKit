281c4d02ca5338ead475cf7fdc830b5ba016dcd5
mironal <miro.gunp@gmail.com>
2022-03-13 23:07:36 +0900

    test GetUsersShowRequestV1

---
+++ Tests/TwitterAPIKitTests/APIv1/Users/GetUsersShowRequestV1Tests.swift
@@ -0,0 +1,42 @@
+import TwitterAPIKit
+import XCTest
+
+class GetUsersShowRequestV1Tests: XCTestCase {
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func test() throws {
+        let req = GetUsersShowRequestV1(
+            user: .userID("uid"),
+            includeEntities: true
+        )
+
+        XCTAssertEqual(req.method, .get)
+        XCTAssertEqual(req.baseURLType, .api)
+        XCTAssertEqual(req.path, "/1.1/users/show.json")
+        XCTAssertEqual(req.bodyContentType, .wwwFormUrlEncoded)
+        AssertEqualAnyDict(
+            req.parameters,
+            [
+                "user_id": "uid",
+                "include_entities": true,
+            ]
+        )
+    }
+
+    func testDefaultArg() throws {
+        let req = GetUsersShowRequestV1(
+            user: .screenName("s")
+        )
+
+        AssertEqualAnyDict(
+            req.parameters,
+            [
+                "screen_name": "s"
+            ]
+        )
+    }
+}
