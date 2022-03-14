829fef39618fcda84f3d5b629f9b67bc24685cd8
mironal <miro.gunp@gmail.com>
2022-03-14 09:01:42 +0900

    test GetAccountSettingsRequestV1

---
+++ Tests/TwitterAPIKitTests/APIv1/Account/GetAccountSettingsRequestV1Tests.swift
@@ -0,0 +1,23 @@
+import TwitterAPIKit
+import XCTest
+
+class GetAccountSettingsRequestV1Tests: XCTestCase {
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func test() throws {
+        let req = GetAccountSettingsRequestV1()
+
+        XCTAssertEqual(req.method, .get)
+        XCTAssertEqual(req.baseURLType, .api)
+        XCTAssertEqual(req.path, "/1.1/account/settings.json")
+        XCTAssertEqual(req.bodyContentType, .wwwFormUrlEncoded)
+        AssertEqualAnyDict(
+            req.parameters,
+            [:]
+        )
+    }
+}
