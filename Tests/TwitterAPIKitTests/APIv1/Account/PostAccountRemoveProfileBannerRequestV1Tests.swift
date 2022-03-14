b617c0b1112d50c5857958b2f2be33c9b25dc2ce
mironal <miro.gunp@gmail.com>
2022-03-14 09:08:02 +0900

    test PostAccountRemoveProfileBannerRequestV1

---
+++ Tests/TwitterAPIKitTests/APIv1/Account/PostAccountRemoveProfileBannerRequestV1Tests.swift
@@ -0,0 +1,23 @@
+import TwitterAPIKit
+import XCTest
+
+class PostAccountRemoveProfileBannerRequestV1Tests: XCTestCase {
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func test() throws {
+        let req = PostAccountRemoveProfileBannerRequestV1()
+
+        XCTAssertEqual(req.method, .post)
+        XCTAssertEqual(req.baseURLType, .api)
+        XCTAssertEqual(req.path, "/1.1/account/remove_profile_banner.json")
+        XCTAssertEqual(req.bodyContentType, .wwwFormUrlEncoded)
+        AssertEqualAnyDict(
+            req.parameters,
+            [:]
+        )
+    }
+}
