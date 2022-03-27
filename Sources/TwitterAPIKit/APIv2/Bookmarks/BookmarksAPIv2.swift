56afbfbb64d15ef9f26006f7abbe5347a1b22046
mironal <miro.gunp@gmail.com>
2022-03-27 10:56:08 +0900

    ad GetUsersBookmarksRequestV2

--- Sources/TwitterAPIKit/APIv2/Bookmarks/BookmarksAPIv2.swift
+++ Sources/TwitterAPIKit/APIv2/Bookmarks/BookmarksAPIv2.swift
@@ -2,8 +2,15 @@ import Foundation
 
 public protocol BookmarksAPIv2 {
 
+    /// https://developer.twitter.com/en/docs/twitter-api/tweets/bookmarks/api-reference/get-users-id-bookmarks
+    func getBookmarks(
+        _ request: GetUsersBookmarksRequestV2
+    ) -> TwitterAPISessionJSONTask
 }
 
 extension TwitterAPIKit.TwitterAPIImplV2: BookmarksAPIv2 {
 
+    func getBookmarks(_ request: GetUsersBookmarksRequestV2) -> TwitterAPISessionJSONTask {
+        return session.send(request)
+    }
 }
