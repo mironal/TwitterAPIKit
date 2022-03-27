b25f0065cdb2a070c0240b8fdfbce7e3427bbef7
mironal <miro.gunp@gmail.com>
2022-03-27 12:09:37 +0900

    test DeleteUsersBookmarksRequestV2

---
+++ Tests/TwitterAPIKitTests/APIv2/Bookmarks/DeleteUsersBookmarksRequestV2Tests.swift
@@ -0,0 +1,26 @@
+import TwitterAPIKit
+import XCTest
+
+class DeleteUsersBookmarksRequestV2Tests: XCTestCase {
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func test() throws {
+        let req = DeleteUsersBookmarksRequestV2(
+            id: "_i_",
+            tweetID: "_t_"
+        )
+
+        XCTAssertEqual(req.method, .delete)
+        XCTAssertEqual(req.baseURLType, .api)
+        XCTAssertEqual(req.path, "/2/users/_i_/bookmarks/_t_")
+        XCTAssertEqual(req.bodyContentType, .wwwFormUrlEncoded)
+        AssertEqualAnyDict(
+            req.parameters,
+            [:]
+        )
+    }
+}
