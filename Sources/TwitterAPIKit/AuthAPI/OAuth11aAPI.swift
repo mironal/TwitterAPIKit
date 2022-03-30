2e6abe0c1551e4b595607d1e9703cd0df1ea5f25
mironal <miro.gunp@gmail.com>
2022-03-30 17:57:23 +0900

    oauth1.1a, oauth2.0

--- Sources/TwitterAPIKit/AuthAPI/OAuth11aAPI.swift
+++ Sources/TwitterAPIKit/AuthAPI/OAuth11aAPI.swift
@@ -1,7 +1,6 @@
 import Foundation
 
-public protocol AuthAPIv1 {
-
+public protocol OAuth11aAPI {
     /// https://developer.twitter.com/en/docs/authentication/api-reference/request_token
     func postOAuthRequestTokenData(
         _ request: PostOAuthRequestTokenRequestV1
@@ -32,29 +31,9 @@ public protocol AuthAPIv1 {
     func postInvalidateAccessToken(
         _ request: PostOAuthInvalidateTokenRequestV1
     ) -> TwitterAPISessionJSONTask
-
-    /// https://developer.twitter.com/en/docs/authentication/api-reference/token
-    func postOAuth2BearerTokenData(
-        _ request: PostOAuth2TokenRequestV1
-    ) -> TwitterAPISessionDataTask
-
-    /// https://developer.twitter.com/en/docs/authentication/api-reference/token
-    func postOAuth2BearerToken(
-        _ request: PostOAuth2TokenRequestV1
-    ) -> TwitterAPISessionSpecializedTask<TwitterOAuth2BearerToken>
-
-    /// https://developer.twitter.com/en/docs/authentication/api-reference/invalidate_bearer_token
-    ///
-    ///May not work. {"errors":[{"code":348,"message":"Client application is not permitted to to invalidate this token."}]}
-    /// https://twittercommunity.com/t/oauth2-invalidate-token-not-working-for-app-only-authentication-tokens/133108
-    /// https://twittercommunity.com/t/invalidate-bearer-client-application-not-permitted/162761
-    func postInvalidateOAuth2BearerToken(
-        _ request: PostOAuth2InvalidateTokenRequestV1
-    ) -> TwitterAPISessionJSONTask
 }
 
-extension TwitterAPIKit.TwitterAPIImplV1: AuthAPIv1 {
-
+extension TwitterAPIKit.TwitterAuthAPIImpl: OAuth11aAPI {
     public func postOAuthRequestTokenData(
         _ request: PostOAuthRequestTokenRequestV1
     ) -> TwitterAPISessionDataTask {
@@ -107,38 +86,6 @@ extension TwitterAPIKit.TwitterAPIImplV1: AuthAPIv1 {
 
     public func postInvalidateAccessToken(
         _ request: PostOAuthInvalidateTokenRequestV1
-    ) -> TwitterAPISessionJSONTask {
-        return session.send(request)
-    }
-
-    public func postOAuth2BearerTokenData(
-        _ request: PostOAuth2TokenRequestV1
-    ) -> TwitterAPISessionDataTask {
-        return session.send(request)
-    }
-
-    func postOAuth2BearerToken(
-        _ request: PostOAuth2TokenRequestV1
-    ) -> TwitterAPISessionSpecializedTask<TwitterOAuth2BearerToken> {
-        return session.send(request)
-            .specialized { data in
-                do {
-                    guard let token = try TwitterOAuth2BearerToken(jsonData: data) else {
-                        throw TwitterAPIKitError.responseSerializeFailed(
-                            reason: .cannotConvert(data: data, toTypeName: "TwitterOAuth2BearerToken")
-                        )
-                    }
-                    return token
-                } catch let error {
-                    throw TwitterAPIKitError.responseSerializeFailed(
-                        reason: .jsonSerializationFailed(error: error)
-                    )
-                }
-            }
-    }
-
-    public func postInvalidateOAuth2BearerToken(
-        _ request: PostOAuth2InvalidateTokenRequestV1
     ) -> TwitterAPISessionJSONTask {
         return session.send(request)
     }
