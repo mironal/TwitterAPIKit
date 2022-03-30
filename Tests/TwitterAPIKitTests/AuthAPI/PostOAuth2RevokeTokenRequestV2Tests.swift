89dc974fca389314414b8cb5bd5392b3afb66959
mironal <miro.gunp@gmail.com>
2022-03-30 18:37:03 +0900

    test PostOAuth2RevokeTokenRequestV2

---
+++ Tests/TwitterAPIKitTests/AuthAPI/PostOAuth2RevokeTokenRequestV2Tests.swift
@@ -0,0 +1,42 @@
+import TwitterAPIKit
+import XCTest
+
+class PostOAuth2RevokeTokenRequestV2Tests: XCTestCase {
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func test() throws {
+        let req = PostOAuth2RevokeTokenRequestV2(
+            token: "_t_",
+            clientID: "_c_"
+        )
+
+        XCTAssertEqual(req.method, .post)
+        XCTAssertEqual(req.baseURLType, .api)
+        XCTAssertEqual(req.path, "/2/oauth2/revoke")
+        XCTAssertEqual(req.bodyContentType, .wwwFormUrlEncoded)
+        AssertEqualAnyDict(
+            req.parameters,
+            [
+                "token": "_t_",
+                "client_id": "_c_",
+            ]
+        )
+    }
+
+    func testDefaultArg() throws {
+        let req = PostOAuth2RevokeTokenRequestV2(
+            token: "_tt_"
+        )
+
+        AssertEqualAnyDict(
+            req.parameters,
+            [
+                "token": "_tt_"
+            ]
+        )
+    }
+}
