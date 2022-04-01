d03d26b7961ae5031108535aa0b70cf7ae49a389
mironal <miro.gunp@gmail.com>
2022-04-01 10:17:49 +0900

    update swift-format

--- BuildTools/Package.swift
+++ BuildTools/Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "BuildTools",
     platforms: [.macOS(.v10_11)],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-format", from: "0.50500.0"),
+        .package(url: "https://github.com/apple/swift-format", from: "0.50600.1"),
     ],
     targets: [.target(name: "BuildTools", path: "")]
 )
