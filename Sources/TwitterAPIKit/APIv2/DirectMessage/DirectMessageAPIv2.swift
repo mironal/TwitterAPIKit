0139a920b3983e2e51bbd2d0ab1ca135d8bd32ed
mironal <miro.gunp@gmail.com>
2022-11-01 10:42:50 +0900

    add DM APIs

--- Sources/TwitterAPIKit/APIv2/DirectMessage/DirectMessageAPIv2.swift
+++ Sources/TwitterAPIKit/APIv2/DirectMessage/DirectMessageAPIv2.swift
@@ -7,4 +7,39 @@ open class DirectMessageAPIv2: TwitterAPIBase {
     ) -> TwitterAPISessionJSONTask {
         return session.send(request)
     }
+
+    /// https://developer.twitter.com/en/docs/twitter-api/direct-messages/lookup/api-reference/get-dm_conversations-with-participant_id-dm_events
+    public func getDmEventsWithParticipantId(
+        _ request: GetDmConversationsWithParticipantIdDmEventsRequestV2
+    ) -> TwitterAPISessionJSONTask {
+        return session.send(request)
+    }
+
+    /// https://developer.twitter.com/en/docs/twitter-api/direct-messages/lookup/api-reference/get-dm_conversations-dm_conversation_id-dm_events
+    public func getDmEventsByConversationsId(
+        _ request: GetDmConversationsIdDmEventsRequestV2
+    ) -> TwitterAPISessionJSONTask {
+        return session.send(request)
+    }
+
+    /// https://developer.twitter.com/en/docs/twitter-api/direct-messages/manage/api-reference/post-dm_conversations-dm_conversation_id-messages
+    public func postDmConversationById(
+        _ request: PostDmConversationByIdRequestV2
+    ) -> TwitterAPISessionJSONTask {
+        return session.send(request)
+    }
+
+    /// https://developer.twitter.com/en/docs/twitter-api/direct-messages/manage/api-reference/post-dm_conversations-with-participant_id-messages
+    public func postDmConversationWithUser(
+        _ request: PostDmConversationWithUserRequestV2
+    ) -> TwitterAPISessionJSONTask {
+        return session.send(request)
+    }
+
+    /// https://developer.twitter.com/en/docs/twitter-api/direct-messages/manage/api-reference/post-dm_conversations
+    public func postDmConversation(
+        _ request: PostDmConversationRequestV2
+    ) -> TwitterAPISessionJSONTask {
+        return session.send(request)
+    }
 }
